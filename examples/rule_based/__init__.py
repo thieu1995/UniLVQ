96675f1732f142aa760e434f220a856075e97793
anh9895 <honganh090895@gmail.com>
2025-05-12 10:16:30 +0700

    Add examples for Optimized LVQ1 algorithm

---
+++ examples/rule_based/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 09:57, 12/05/2025 ----------%
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
